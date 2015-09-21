ca1fffc6d67adf496ed3650943fe0cd4c362537b
Robert Rajakone <robie2011@gmail.com>
2015-09-21 11:02:42 +0200

    eaf DI CoC Convention over Configuration

---
+++ eaf/springioc.hello.config.ab4/src/main/java/edu/spring/domain/CoCConfiguration.java
@@ -0,0 +1,11 @@
+package edu.spring.domain;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"edu.spring.domain"})
+public class CoCConfiguration {
+
+}
--- eaf/springioc.hello.config.ab4/src/test/java/edu/spring/convention/HelloWorldConventionTest.java
+++ eaf/springioc.hello.config.ab4/src/test/java/edu/spring/convention/HelloWorldConventionTest.java
@@ -6,12 +6,14 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import edu.spring.domain.MessageProvider;
 import edu.spring.domain.MessageRenderer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {edu.spring.domain.CoCConfiguration.class})
 public class HelloWorldConventionTest {
 	@Autowired
 	private MessageProvider messageProvider;
